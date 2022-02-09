ec2519f22b60ec843bdcfe89d9b9bd0d01af2eaf
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2022-02-09 18:57:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// https://teachablemachine.withgoogle.com/models/3cRKXuWJ0/
+ Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+ });
+ Webcam.attach( "#camera" );
+function takepicture(){
+    Webcam.snap( function(data_uri) {
+        // display results in page
+        document.getElementById("result").innerHTML = 
+         '<img id="image1" src="'+data_uri+'"/>';
+    } );
+}
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XhkGdYWsd/model.json",modelLoaded);
+function modelLoaded(){
+    console.log("succcessfuly loaded");
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    speak1="first prediction is "+prediction1;
+    speak2="second prediction is "+prediction2;
+    var speakthis=new SpeechSynthesisUtterance(speak1+speak2);
+    synth.speak(speakthis);
+}
